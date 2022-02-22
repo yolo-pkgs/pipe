4b22f49d0c07d4c405151c8f16f03476d19d958a
Gleb Buzin <qufiwefefwoyn@gmail.com>
2022-02-22 22:14:50 +0300

    docs(pipe): add docs

--- pipe.go
+++ pipe.go
@@ -1,3 +1,4 @@
+// Package pipe implements a Pipe.
 package pipe
 
 type Pipe struct {
@@ -5,21 +6,25 @@ type Pipe struct {
 	funcs []func() error
 }
 
+// New constructs an empty Pipe.
 func New() *Pipe {
 	return &Pipe{
 		onErr: func(e error) { /* do nothing */ },
 	}
 }
 
+// OnErr is a convinience function to handle an error.
 func (p *Pipe) OnErr(handler func(error)) {
 	p.onErr = handler
 }
 
+// Next appends the provided funcion to the Pipe.
 func (p *Pipe) Next(f func() error) *Pipe {
 	p.funcs = append(p.funcs, f)
 	return p
 }
 
+// Do executes the Pipe.
 func (p *Pipe) Do() error {
 	for _, f := range p.funcs {
 		if err := f(); err != nil {
